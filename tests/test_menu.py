7f2540c0016a0892259917267e80f8f640b55dd6
Francesca <francesca@pop-os.localdomain>
2023-12-16 17:09:22 +0100

    Fixed MenuDescription name conflict in tests

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -3,33 +3,33 @@ from unittest.mock import patch, call, Mock
 import pytest
 from valid8 import ValidationError
 
-from movie.menu import Description, Key, Entry, Menu
+from movie.menu import MenuDescription, Key, Entry, Menu
 
 
 def test_description_must_be_string():
-    Description('ok')
+    MenuDescription('ok')
     with pytest.raises(TypeError):
-        Description(0)
+        MenuDescription(0)
     with pytest.raises(TypeError):
-        Description(None)
+        MenuDescription(None)
 
 
 def test_description_must_be_non_empty_string():
-    Description('test')
+    MenuDescription('test')
     with pytest.raises(ValidationError):
-        Description('')
+        MenuDescription('')
 
 
 def test_description_must_not_exceed_1000_chars():
-    Description('a'*1000)
+    MenuDescription('a'*1000)
     with pytest.raises(ValidationError):
-        Description('a'*1001)
+        MenuDescription('a'*1001)
 
 
 def test_description_must_not_contain_special_chars():
     for special_char in ['\n', '\r', '*', '^', '$']:
         with pytest.raises(ValidationError):
-            Description(special_char)
+            MenuDescription(special_char)
 
 
 def test_key_cannot_be_empty():
@@ -50,26 +50,26 @@ def test_key_cannot_contain_special_chars():
 
 def test_entry_on_selected():
     mocked_on_selected = Mock()
-    entry = Entry(Key('1'), Description('Say hi'), on_selected=lambda: mocked_on_selected())
+    entry = Entry(Key('1'), MenuDescription('Say hi'), on_selected=lambda: mocked_on_selected())
     entry.on_selected()
     mocked_on_selected.assert_called_once()
 
 
 @patch('builtins.print')
 def test_entry_on_selected_print_something(mocked_print):
-    entry = Entry(Key('1'), Description('Say hi'), on_selected=lambda: print('hi'))
+    entry = Entry(Key('1'), MenuDescription('Say hi'), on_selected=lambda: print('hi'))
     entry.on_selected()
     assert mocked_print.mock_calls == [call('hi')]
 
 
 def test_menu_builder_cannot_create_empty_menu():
-    menu_builder = Menu.Builder(Description('a description'))
+    menu_builder = Menu.Builder(MenuDescription('a MenuDescription'))
     with pytest.raises(ValidationError):
         menu_builder.build()
 
 
 def test_menu_builder_cannot_create_menu_without_exit():
-    menu_builder = Menu.Builder(Description('a description'))
+    menu_builder = Menu.Builder(MenuDescription('a MenuDescription'))
     with pytest.raises(ValidationError):
         menu_builder.build()
     menu_builder.with_entry(Entry.create('1', 'exit', is_exit=True))
@@ -77,7 +77,7 @@ def test_menu_builder_cannot_create_menu_without_exit():
 
 
 def test_menu_builder_cannot_call_two_times_build():
-    menu_builder = Menu.Builder(Description('a description'))
+    menu_builder = Menu.Builder(MenuDescription('a MenuDescription'))
     menu_builder.with_entry(Entry.create('1', 'first entry', is_exit=True))
     menu_builder.build()
     with pytest.raises(ValidationError):
@@ -85,7 +85,7 @@ def test_menu_builder_cannot_call_two_times_build():
 
 
 def test_menu_does_not_contain_duplicates():
-    menu_builder = Menu.Builder(Description('a description'))
+    menu_builder = Menu.Builder(MenuDescription('a MenuDescription'))
     menu_builder.with_entry(Entry.create('1', 'first entry'))
     with pytest.raises(ValidationError):
         menu_builder.with_entry(Entry.create('1', 'first entry'))
@@ -94,7 +94,7 @@ def test_menu_does_not_contain_duplicates():
 @patch('builtins.input', side_effect=['1', '0'])
 @patch('builtins.print')
 def test_menu_selection_call_on_selected(mocked_print, mocked_input):
-    menu = Menu.Builder(Description('a description'))\
+    menu = Menu.Builder(MenuDescription('a MenuDescription'))\
         .with_entry(Entry.create('1', 'first entry', on_selected=lambda: print('first entry selected')))\
         .with_entry(Entry.create('0', 'exit', is_exit=True))\
         .build()
@@ -106,7 +106,7 @@ def test_menu_selection_call_on_selected(mocked_print, mocked_input):
 @patch('builtins.input', side_effect=['-1', '0'])
 @patch('builtins.print')
 def test_menu_selection_on_wrong_key(mocked_print, mocked_input):
-    menu = Menu.Builder(Description('a description'))\
+    menu = Menu.Builder(MenuDescription('a MenuDescription'))\
         .with_entry(Entry.create('1', 'first entry', on_selected=lambda: print('first entry selected')))\
         .with_entry(Entry.create('0', 'exit', is_exit=True))\
         .build()
